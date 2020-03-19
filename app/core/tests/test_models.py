c1cfe3969b95502db0c133c29c068d40020cd8b0
SUTHARRAM <rls42847@gmail.com>
2020-03-19 12:28:41 +0530

    Re-facture

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -39,14 +39,34 @@ class ModelTests(TestCase):
             'test123'
         )
 
+        # print('+++++++++++++++++++++++++++++++++++')
+        # print(user)
+        # print('+++++++++++++++++++++++++++++++++++')
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-def test_tag_str(self):
-    """Test the tag string representation"""
-    tag = models.Tag.objects.create(
-        user=sample_user(),
-        name='Vegan'
-    )
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
 
-    self.assertEqual(str(tag), tag.name)
+        self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name= 'Cucumber'
+        )
+        # print('+++++++++++++++++++++++++++++++++++')
+        # print(type(str(ingredient)))
+        # print(type(ingredient.name))
+        # print('+++++++++++++++++++++++++++++++++++')
+
+        self.assertEqual(str(ingredient), ingredient.name)
+
+
+
